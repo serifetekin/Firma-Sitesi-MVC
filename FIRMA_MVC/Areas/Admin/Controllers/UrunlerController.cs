f375a10b1b935027ad6bb33002d0d8beab824bed
AYBU <AYBU@DESKTOP-UO28OLR>
2019-09-10 20:04:05 +0300

    10.09.2019 son ders

---
+++ FIRMA_MVC/Areas/Admin/Controllers/UrunlerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FIRMA_MVC.Models;
+namespace FIRMA_MVC.Areas.Admin.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        FIRMAMODEL db = new FIRMAMODEL();
+        // GET: Admin/Urunler
+        public ActionResult Index()
+        {
+            List<URUN> liste = db.URUNs.ToList();
+            return View(liste);
+        }
+    }
+}
